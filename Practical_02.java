14ea0d02b9dd7b60989d149627668265ee12cedb
dinnu-netha <nishveshteja.1477@gmail.com>
2026-07-14 10:52:46 +0530

    Add files via upload

---
+++ Practical_02.java
@@ -0,0 +1,11 @@
+public class Practical_02 {
+    public static void main(String[] args){
+        int a = 12;
+        int b = 34;
+        System.out.println("Number 1 is: " + a);
+        System.out.println("Number 2 is: " + b);
+        int c = a+b;
+        System.out.println("Sum of a & b  is: " + c);
+        
+    }
+}
